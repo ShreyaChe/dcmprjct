1522a109d30e604d9e26224d57a82ea102113e83
ShreyaChe <45650436+ShreyaChe@users.noreply.github.com>
2019-01-13 20:05:31 +0530

    Add files via upload

--- dictag.py
+++ dictag.py
@@ -2,8 +2,10 @@ import pydicom
 ds = pydicom.read_file("ttfm.dcm")
 bs = pydicom.read_file("bmode.dcm")
 f = open("dicomtag.txt", "a")
-f.write('****ttfm docom tags*****\n')
-f.write('\n'.join(ds.dir()))
+f.write('****ttfm docom tags:Value*****\n')
+for i in ds.dir():
+   f.write(f'{i:30} : {ds.data_element(i).repval:100}'+'\n')
 g = open("bicomtag.txt", "a")
-g.write('****bmode dicom tags*****\n')
-g.write('\n'.join(bs.dir()))
+g.write('****bmode dicom tags: Value*****\n')
+for j in bs.dir():
+    g.write(f'{j:30} : {bs.data_element(j).repval:100}'+'\n')
